656f0f021ba602548b11347afc4f859ee7adc668
aserrr <aserrr@ya.ru>
2014-05-17 17:50:45 +0400

    Добавил стандартный код для работы с Parse Framework

--- Moto64/Moto64/MTAppDelegate.m
+++ Moto64/Moto64/MTAppDelegate.m
@@ -10,6 +10,7 @@
 #import "MTMasterViewController.h"
 #import "AFNetworkActivityIndicatorManager.h"
 #import "MTRSSDataSource.h"
+#import <Parse/Parse.h>
 
 @implementation MTAppDelegate
 
@@ -19,6 +20,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    
+    [Parse setApplicationId:@"YHAuARF6h5GNjj3vnn0WMv6McjITUQTGtzsRHGgp"
+                  clientKey:@"QRlb6AqA4VIwglloat1rVkvdnWgJjpDvnQ45A2dV"];
+    
     // Override point for customization after application launch.
     UINavigationController *navigationController = (UINavigationController *)self.window.rootViewController;
     MTMasterViewController *controller = (MTMasterViewController *)navigationController.topViewController;
@@ -85,18 +90,17 @@
 
 // Delegation methods
 
-- (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)devToken
+- (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
-    
-    const void *devTokenBytes = [devToken bytes];
-    //send device token to the push provider
-    
-    NSLog(@"My token is: %@", devTokenBytes);
-    
+    // Store the deviceToken in the current installation and save it to Parse.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    [currentInstallation setDeviceTokenFromData:deviceToken];
+    [currentInstallation saveInBackground];
 }
 
 - (void) application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
 {
+    [PFPush handlePush:userInfo];
     
     if ([application applicationState] == UIApplicationStateInactive) {
         // user tapped the action button
@@ -115,28 +119,6 @@
     
     NSLog(@"Error in registration. Error: %@", err);
     
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
