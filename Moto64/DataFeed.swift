c8cf7232e0acbd7b06b5a97709f3d25221f2cc5e
AseR <aser@central-office.net>
2015-04-16 23:54:16 +0300

    -

--- Moto64/DataFeed.swift
+++ Moto64/DataFeed.swift
@@ -102,12 +102,6 @@ class DataFeed: NSObject, NSXMLParserDelegate
                 currentItem.title = title
         case "link": currentItem.link = string!
             case "description":
-//                var descr = string!.stringByReplacingOccurrencesOfString("<[^>]+>", withString: "", options: .RegularExpressionSearch, range: nil)
-//                descr = descr.stringByReplacingOccurrencesOfString("&nbsp;", withString: "", options: .RegularExpressionSearch, range: nil)
-//                for i in 1...5 {
-//                    descr = descr.stringByReplacingOccurrencesOfString("\n\n", withString: "\n", options: .RegularExpressionSearch, range: nil)
-//                }
-//                currentItem.descr = descr.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
                 var descr = stripHTML(string!)
                 currentItem.descr = stripHTML(descr)
 
